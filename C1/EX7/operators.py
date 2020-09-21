92f92f4404976dedfed33a9915e7f6d720399f2b
Jonas Claes <jonasclaes2001@outlook.com>
2020-09-21 11:30:06 +0200

    C1EX7: Operators

---
+++ C1/EX7/operators.py
@@ -0,0 +1,15 @@
+number = 15
+
+print("The starting number is " + str(number))
+number *= 10
+print(number)
+number += 5
+print(number)
+number -= 7
+print(number)
+number += 1
+print(number)
+number *= 100
+print(number)
+number /= 2
+print(int(number))
