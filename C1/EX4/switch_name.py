5f0e7c87a04b0474196ec86a2ddf49a09f7ce606
Jonas Claes <jonasclaes2001@outlook.com>
2020-09-21 11:19:39 +0200

    C1EX4: Switch names

---
+++ C1/EX4/switch_name.py
@@ -0,0 +1,6 @@
+# Get name
+first_name = input("Enter the first name: ")
+second_name = input("Enter the second name: ")
+
+print("Before changing:", first_name, second_name)
+print("Before changing:", second_name, first_name)
