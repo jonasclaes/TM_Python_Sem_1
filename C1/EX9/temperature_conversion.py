eb88896f815624fd406e3e7db5acdafab406051c
Jonas Claes <jonasclaes2001@outlook.com>
2020-09-21 11:38:27 +0200

    C1EX9: Temperature conversion

---
+++ C1/EX9/temperature_conversion.py
@@ -0,0 +1,6 @@
+# Get temperature in degrees celsius
+temperature_celsius = float(input("Enter the number of degrees Celsius: "))
+
+temperature_fahrenheit = temperature_celsius * 9/5 + 32
+
+print(str(temperature_celsius) + " degrees Celsius = " + str(temperature_fahrenheit) + " degrees Fahrenheit")
