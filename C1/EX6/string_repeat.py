73cf4211d2cb4a86406361bf513f0929990e3a19
Jonas Claes <jonasclaes2001@outlook.com>
2020-09-21 11:26:30 +0200

    C1EX6: String repeat

---
+++ C1/EX6/string_repeat.py
@@ -0,0 +1,5 @@
+# Get string to repeat and amount of times
+amount_of_times_to_repeat = int(input("Amount of times to repeat: "))
+string_to_repeat = input("Text to repeat: ")
+
+print(str(amount_of_times_to_repeat) + " times your text: " + string_to_repeat * amount_of_times_to_repeat)
