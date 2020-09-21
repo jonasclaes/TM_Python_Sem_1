5ff60f9a9009bc3257f63a295b6079c448319ca7
Jonas Claes <jonasclaes2001@outlook.com>
2020-09-21 11:08:38 +0200

    C1EX1: Address label

---
+++ C1/EX1/address_label.py
@@ -0,0 +1,12 @@
+# Get info
+lastname = input("surname: ")
+firstname = input("firstname: ")
+street = input("street: ")
+number = input("number: ")
+zip_code = input("zip code: ")
+city = input("city: ")
+
+# Print label
+print(firstname, lastname)
+print(street, number)
+print(zip_code, city)
