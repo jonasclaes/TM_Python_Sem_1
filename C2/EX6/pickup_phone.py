8722449547a7d7c2d7cf559e4592ec733c93a6a8
Jonas Claes <jonasclaes2001@outlook.com>
2020-09-28 10:40:56 +0200

    C2EX6: Pickup phone

---
+++ C2/EX6/pickup_phone.py
@@ -0,0 +1,10 @@
+is_morning = True
+is_mother = False
+is_asleep = False
+
+if is_morning and not is_mother:
+    print("I'm not answering my phone")
+elif is_asleep:
+    print("I'm not answering my phone")
+else:
+    print("I'm answering my phone")
