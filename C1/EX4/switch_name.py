9def2891c37e57cc64fdbc87dadc53a4f42c1b91
Jonas Claes <jonasclaes2001@outlook.com>
2020-09-28 08:53:22 +0200

    Fixed

--- C1/EX4/switch_name.py
+++ C1/EX4/switch_name.py
@@ -3,4 +3,7 @@ first_name = input("Enter the first name: ")
 second_name = input("Enter the second name: ")
 
 print("Before changing:", first_name, second_name)
-print("Before changing:", second_name, first_name)
+
+first_name, second_name = second_name, first_name
+
+print("After changing:", first_name, second_name)
